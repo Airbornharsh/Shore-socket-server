f478e24280b7252b1da31e4192859584c5a4cfb7
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-04-01 00:19:14 +0530

    Added an push-notification Api

--- Routes.ts
+++ Routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import check from "./src/functions/check";
+const {
+  SendPushNotification,
+} = require("./src/controllers/push-notifications-controller");
 
 const Router = express.Router();
 
 Router.get("/", check);
+Router.post("/push-notification", SendPushNotification);
 
 export default Router;
